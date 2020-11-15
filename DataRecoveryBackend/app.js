594fa225a327bb344d2272711e2c8579bed60a58
Cassandra <cassandra.schilling00@gmail.com>
2020-11-15 15:22:51 +0100

    Updated order component

--- DataRecoveryBackend/app.js
+++ DataRecoveryBackend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config/app.env'});
 
 // NICHT ENTFERNEN
